2092581de9033d944bcbbe2b71d3d53d7cdd6663
dnaniel213 <dnaniel213@gmail.com>
2023-08-03 23:18:53 +0800

    - caplog finish

--- Capriccioso/Utilities/CapLog.cs
+++ Capriccioso/Utilities/CapLog.cs
@@ -30,9 +30,43 @@ namespace Capriccioso
 
         public void LogInfo(string message, Object context = null, [CallerFilePath] string path = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string caller = null)
         {
-            
+            Init();
+            string file = Path.GetFileName(path);
+            string log = $"<color={LogColors.Info}><b>[{file} @ {caller}():{lineNumber}]</b> INFO </color> - {message}";
+            Debug.Log(log, context);
         }
         
+        public void LogSuccess(string message, Object context = null, [CallerFilePath] string path = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string caller = null)
+        {
+            Init();
+            string file = Path.GetFileName(path);
+            string log = $"<color={LogColors.Success}><b>[{file} @ {caller}():{lineNumber}]</b> GOOD </color> - {message}";
+            Debug.Log(log, context);
+        }
+
+        public void LogWarning(string message, Object context = null, [CallerFilePath] string path = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string caller = null)
+        {
+            Init();
+            string file = Path.GetFileName(path);
+            string log = $"<color={LogColors.Warning}><b>[{file} @ {caller}():{lineNumber}]</b> WARN </color> - {message}";
+            Debug.Log(log, context);
+        }
+
+        public void LogError(string message, Object context = null, [CallerFilePath] string path = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string caller = null)
+        {
+            Init();
+            string file = Path.GetFileName(path);
+            string log = $"<color={LogColors.Error}><b>[{file} @ {caller}():{lineNumber}]</b> ERRR </color> - {message}";
+            Debug.Log(log, context);
+        }
+
+        public void LogAssert(bool condition, string message, Object context = null, [CallerFilePath] string path = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string caller = null)
+        {
+            Init();
+            string file = Path.GetFileName(path);
+            string log = $"<color={LogColors.Error}><b>[{file} @ {caller}():{lineNumber}]</b> ERRR </color> - {message}";
+            Debug.LogAssert(condition, message , context);
+        }
     }
 
 }
